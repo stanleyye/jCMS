56449d73733014c4aadcc3a8a974b7f163b7b702
Stanley Ye <stanleyye1996@gmail.com>
2017-05-25 22:55:17 -0700

    fixed FOREIGN key constraint error for schema.sql

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ The frontend side uses Babel, React and Webpack.
 The build tool and web server include Maven and Tomcat. The data and contents are stored using mySQL.
 
 ## Setting up
-Make sure that you have mySQL 5.x.x installed. Then enter your credentials into the config.properties file, which jCMS will use to write into the DB.
+Make sure that you have mySQL 5.x.x installed.
+Create a mySQL database called 'jcms' (you can change the name but make sure to update it in
+application.properities under datasource url)
+Then enter your credentials into the config.properties file, which jCMS will use to write into the DB.
 
 ## Building and deploying
 ```bash
 mvn clean spring-boot:run
 
-For more logging purposes
+# For more logging purposes
 mvn clean spring-boot:run -X
 ```
--- src/main/resources/data/mysql/schema.sql
+++ src/main/resources/data/mysql/schema.sql
@@ -3,23 +3,23 @@
  * Create the database schema for the application.
  */
 
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` BIGINT(20) unsigned NOT NULL auto_increment,
-  `publicationDate` TIMESTAMP DEFAULT NOW(),
-  `title` VARCHAR(100) DEFAULT NULL,
-  `summary` VARCHAR(255) DEFAULT NULL,
-  `content` VARCHAR(255) DEFAULT NULL,
-  `author` BIGINT(20) NOT NULL DEFAULT 0,
-  PRIMARY KEY(`id`),
-  FOREIGN KEY(`author`) REFERENCES users(`id`)
+CREATE TABLE IF NOT EXISTS users (
+  id BIGINT(20) unsigned NOT NULL auto_increment,
+  creationDate TIMESTAMP DEFAULT NOW(),
+  username VARCHAR(25) DEFAULT NULL,
+  email VARCHAR(80) DEFAULT NULL,
+  password VARCHAR(255) DEFAULT NULL,
+  userLevel INT NOT NULL DEFAULT 0,
+  PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` BIGINT(20) unsigned NOT NULL auto_increment,
-  `creationDate` TIMESTAMP DEFAULT NOW(),
-  `username` VARCHAR(25) DEFAULT NULL,
-  `email` VARCHAR(80) DEFAULT NULL,
-  `password` VARCHAR(255) DEFAULT NULL,
-  `userLevel` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS posts (
+  id BIGINT(20) unsigned NOT NULL auto_increment,
+  publicationDate TIMESTAMP DEFAULT NOW(),
+  title VARCHAR(100) DEFAULT NULL,
+  summary VARCHAR(255) DEFAULT NULL,
+  content VARCHAR(255) DEFAULT NULL,
+  author BIGINT(20) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY(id),
+  FOREIGN KEY(author) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
