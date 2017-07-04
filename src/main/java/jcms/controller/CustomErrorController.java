2b3799681cd2e106ef7fcb19070f1abd81836b8d
Stanley Ye <stanleyye1996@gmail.com>
2017-07-03 20:59:50 -0700

    very minor change of import order

--- src/main/java/jcms/controller/CustomErrorController.java
+++ src/main/java/jcms/controller/CustomErrorController.java
@@ -1,11 +1,10 @@
 package jcms.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.boot.autoconfigure.web.ErrorController;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller for unhandled errors (such as unhandled route errors).
