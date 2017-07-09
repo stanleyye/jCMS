83f3671aac88f8230bacff6c1d98de7bc1829bd1
Stanley Ye <stanleyye1996@gmail.com>
2017-07-08 19:18:23 -0700

    Added admin + home page

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router-dom'
 
-// import BottomNavBar from './navigation/bottom-nav-bar'
-// import NavHeader from './navigation/nav-header'
+import AdminPage from './pages/admin-page'
+import HomePage from './pages/home-page'
 import LoginPage from './pages/login-page'
 import RegisterPage from './pages/register-page'
-
-const Home = () => (
-		<h1>Home page</h1>
-)
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Route exact path='/' component={Home}/>
+				<Route exact path='/' component={HomePage}/>
+				<Route path='/admin' component={AdminPage}
 				<Route path='/login' component={LoginPage}/>
 				<Route path='/register' component={RegisterPage}/>
 			</div>
---
+++ src/main/js/pages/admin-page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class AdminPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Admin Page</h1>
+			</div>
+		);
+	}
+}
+
+export default AdminPage
