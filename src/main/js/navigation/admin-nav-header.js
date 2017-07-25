7fdaa2c4a4117e3b0e0dedca037ad673738962db
Stanley Ye <stanleyye1996@gmail.com>
2017-07-25 16:43:22 -0700

    Added client side JWT decoding module

--- src/main/js/navigation/admin-nav-header.js
+++ src/main/js/navigation/admin-nav-header.js
@@ -1,35 +1,60 @@
+import jwtDecode from 'jwt-decode'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const AdminNavHeader = () => (
-	<nav className="navbar navbar-default">
-		<div className="container-fluid">
-			<div className="navbar-header">
-				<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span className="sr-only">Toggle navigation</span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-				</button>
-				<a className="navbar-brand" href="#">Brand</a>
-			</div>
+// TODO: get the token value that is stored inside the 'jCMSCookie' cookie
+const getCookie = (cookieName) => {
 
-			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul className="nav navbar-nav navbar-right">
-					<li className="dropdown">
-						<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-						<ul className="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" className="divider"></li>
-							<li><a href="#">Separated link</a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>{/* /.navbar-collapse */}
-		</div>{/* /.container-fluid */}
-	</nav>
-)
+}
+
+class AdminNavHeader extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			current_user: {}
+		}
+	}
+
+	componentDidMount() {
+		// console.log(jwtDecode(getCookie('jCMSCookie')));
+	}
+
+	render() {
+		return (
+			<div>
+				<nav className="navbar navbar-default">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span className="sr-only">Toggle navigation</span>
+								<span className="icon-bar"></span>
+								<span className="icon-bar"></span>
+								<span className="icon-bar"></span>
+							</button>
+							<a className="navbar-brand" href="#">Brand</a>
+						</div>
+
+						<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul className="nav navbar-nav navbar-right">
+								<li className="dropdown">
+									<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+										Dropdown <span className="caret"></span>
+									</a>
+									<ul className="dropdown-menu">
+										<li><a href="#">Profile</a></li>
+										<li><a href="#">Settings</a></li>
+										<li role="separator" className="divider"></li>
+										<li><a href="#">Logout</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+}
 
 export default AdminNavHeader;
